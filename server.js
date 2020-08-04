185a1fd222a3d6647f7c8ed513940471f1fd753e
SHUBHAM KUMAR <59328879+shubham06082001@users.noreply.github.com>
2020-08-04 12:20:48 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcryptjs");
+const cors = require("cors");
+
+const knex = require("knex");
+
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "shubhamkumar",
+    password: "",
+    database: "smart-brain",
+  },
+});
+
+const app = express();
+/*
+
+// / --> res = this is working
+// /signin --> POST = success/fail
+// /register --> POST = user
+// /profile/:userId --> GET = user
+// /image --> PUT = user
+
+*/
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send(database.users);
+});
+
+app.post("/signin", (req, res) => {
+  signin.handleSignin(req, res, db, bcrypt);
+});
+
+app.post("/register", register.handleRegister(db, bcrypt));
+
+app.get("/profile/:id", (req, res) => {
+  profile.handleProfileGet(req, res, db);
+});
+
+app.put("/image", (req, res) => {
+  image.handleImage(req, res, db);
+});
+app.post("/imageurl", (req, res) => {
+  image.handleApiCall(req, res);
+});
+
+app.listen(3000, () => {
+  console.log("App is running on port 3000");
+});
